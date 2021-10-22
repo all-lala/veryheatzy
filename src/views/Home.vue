8e7c5b9d2b6578b56e15c8c7b00bd49ecf4575ef
all-lala <aurelien.laby@gmail.com>
2021-10-23 00:45:21 +0200

    correction

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,7 +10,6 @@ import { Component, Vue } from "vue-property-decorator";
 import LoginFormComponent from "@/components/LoginFormComponent.vue";
 import DeviceListComponent from "@/components/DeviceListComponent.vue";
 import ScheduleDayComponent from "@/components/ScheduleDayComponent.vue";
-import { StoreActions } from "@/store";
 
 @Component({
   components: {
@@ -20,15 +19,6 @@ import { StoreActions } from "@/store";
   },
 })
 export default class Home extends Vue {
-  beforeMount(): void {
-    const query = this.$route.query;
-    const username = query.username;
-    const password = query.password;
-    if (username && password) {
-      this.$store.dispatch(StoreActions.getToken, { username, password });
-    }
-  }
-
   get token(): string {
     return this.$store.state.token;
   }
